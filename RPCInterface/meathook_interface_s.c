114b7b174328684e45caaf89e9dad3df716a24df
Scorp0rX0r <nop_jne@hotmail.com>
2020-09-21 07:25:23 -0700

    ETH: Meathook interface

---
+++ RPCInterface/meathook_interface_s.c
@@ -0,0 +1,360 @@
+
+
+/* this ALWAYS GENERATED file contains the RPC server stubs */
+
+
+ /* File created by MIDL compiler version 8.01.0622 */
+/* at Mon Jan 18 19:14:07 2038
+ */
+/* Compiler settings for meathook_interface.idl:
+    Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 8.01.0622 
+    protocol : dce , ms_ext, c_ext, robust
+    error checks: allocation ref bounds_check enum stub_data 
+    VC __declspec() decoration level: 
+         __declspec(uuid()), __declspec(selectany), __declspec(novtable)
+         DECLSPEC_UUID(), MIDL_INTERFACE()
+*/
+/* @@MIDL_FILE_HEADING(  ) */
+
+#if defined(_M_AMD64)
+
+
+#pragma warning( disable: 4049 )  /* more than 64k source lines */
+#if _MSC_VER >= 1200
+#pragma warning(push)
+#endif
+
+#pragma warning( disable: 4211 )  /* redefine extern to static */
+#pragma warning( disable: 4232 )  /* dllimport identity*/
+#pragma warning( disable: 4024 )  /* array to pointer mapping*/
+#pragma warning( disable: 4100 ) /* unreferenced arguments in x86 call */
+
+#pragma optimize("", off ) 
+
+#include <string.h>
+#include "meathook_interface.h"
+
+#define TYPE_FORMAT_STRING_SIZE   43                                
+#define PROC_FORMAT_STRING_SIZE   163                               
+#define EXPR_FORMAT_STRING_SIZE   1                                 
+#define TRANSMIT_AS_TABLE_SIZE    0            
+#define WIRE_MARSHAL_TABLE_SIZE   0            
+
+typedef struct _meathook_interface_MIDL_TYPE_FORMAT_STRING
+    {
+    short          Pad;
+    unsigned char  Format[ TYPE_FORMAT_STRING_SIZE ];
+    } meathook_interface_MIDL_TYPE_FORMAT_STRING;
+
+typedef struct _meathook_interface_MIDL_PROC_FORMAT_STRING
+    {
+    short          Pad;
+    unsigned char  Format[ PROC_FORMAT_STRING_SIZE ];
+    } meathook_interface_MIDL_PROC_FORMAT_STRING;
+
+typedef struct _meathook_interface_MIDL_EXPR_FORMAT_STRING
+    {
+    long          Pad;
+    unsigned char  Format[ EXPR_FORMAT_STRING_SIZE ];
+    } meathook_interface_MIDL_EXPR_FORMAT_STRING;
+
+
+static const RPC_SYNTAX_IDENTIFIER  _RpcTransferSyntax = 
+{{0x8A885D04,0x1CEB,0x11C9,{0x9F,0xE8,0x08,0x00,0x2B,0x10,0x48,0x60}},{2,0}};
+
+extern const meathook_interface_MIDL_TYPE_FORMAT_STRING meathook_interface__MIDL_TypeFormatString;
+extern const meathook_interface_MIDL_PROC_FORMAT_STRING meathook_interface__MIDL_ProcFormatString;
+extern const meathook_interface_MIDL_EXPR_FORMAT_STRING meathook_interface__MIDL_ExprFormatString;
+
+/* Standard interface: meathook_interface, ver. 1.0,
+   GUID={0x1c9ca7c8,0xd421,0x482d,{0xb8,0x5d,0x79,0xfa,0xc3,0x3b,0x26,0x58}} */
+
+
+extern const MIDL_SERVER_INFO meathook_interface_ServerInfo;
+
+extern const RPC_DISPATCH_TABLE meathook_interface_v1_0_DispatchTable;
+
+static const RPC_SERVER_INTERFACE meathook_interface___RpcServerInterface =
+    {
+    sizeof(RPC_SERVER_INTERFACE),
+    {{0x1c9ca7c8,0xd421,0x482d,{0xb8,0x5d,0x79,0xfa,0xc3,0x3b,0x26,0x58}},{1,0}},
+    {{0x8A885D04,0x1CEB,0x11C9,{0x9F,0xE8,0x08,0x00,0x2B,0x10,0x48,0x60}},{2,0}},
+    (RPC_DISPATCH_TABLE*)&meathook_interface_v1_0_DispatchTable,
+    0,
+    0,
+    0,
+    &meathook_interface_ServerInfo,
+    0x04000000
+    };
+RPC_IF_HANDLE meathook_interface_v1_0_s_ifspec = (RPC_IF_HANDLE)& meathook_interface___RpcServerInterface;
+
+extern const MIDL_STUB_DESC meathook_interface_StubDesc;
+
+
+#if !defined(__RPC_WIN64__)
+#error  Invalid build platform for this stub.
+#endif
+
+#if !(TARGET_IS_NT50_OR_LATER)
+#error You need Windows 2000 or later to run this stub because it uses these features:
+#error   /robust command line switch.
+#error However, your C/C++ compilation flags indicate you intend to run this app on earlier systems.
+#error This app will fail with the RPC_X_WRONG_STUB_VERSION error.
+#endif
+
+
+static const meathook_interface_MIDL_PROC_FORMAT_STRING meathook_interface__MIDL_ProcFormatString =
+    {
+        0,
+        {
+
+	/* Procedure ExecuteConsoleCommand */
+
+			0x0,		/* 0 */
+			0x48,		/* Old Flags:  */
+/*  2 */	NdrFcLong( 0x0 ),	/* 0 */
+/*  6 */	NdrFcShort( 0x0 ),	/* 0 */
+/*  8 */	NdrFcShort( 0x10 ),	/* x86 Stack size/offset = 16 */
+/* 10 */	0x32,		/* FC_BIND_PRIMITIVE */
+			0x0,		/* 0 */
+/* 12 */	NdrFcShort( 0x0 ),	/* x86 Stack size/offset = 0 */
+/* 14 */	NdrFcShort( 0x0 ),	/* 0 */
+/* 16 */	NdrFcShort( 0x0 ),	/* 0 */
+/* 18 */	0x42,		/* Oi2 Flags:  clt must size, has ext, */
+			0x1,		/* 1 */
+/* 20 */	0xa,		/* 10 */
+			0x1,		/* Ext Flags:  new corr desc, */
+/* 22 */	NdrFcShort( 0x0 ),	/* 0 */
+/* 24 */	NdrFcShort( 0x0 ),	/* 0 */
+/* 26 */	NdrFcShort( 0x0 ),	/* 0 */
+/* 28 */	NdrFcShort( 0x0 ),	/* 0 */
+
+	/* Parameter pszString */
+
+/* 30 */	NdrFcShort( 0x10b ),	/* Flags:  must size, must free, in, simple ref, */
+/* 32 */	NdrFcShort( 0x8 ),	/* x86 Stack size/offset = 8 */
+/* 34 */	NdrFcShort( 0x4 ),	/* Type Offset=4 */
+
+	/* Procedure PushEntitiesFile */
+
+/* 36 */	0x0,		/* 0 */
+			0x48,		/* Old Flags:  */
+/* 38 */	NdrFcLong( 0x0 ),	/* 0 */
+/* 42 */	NdrFcShort( 0x1 ),	/* 1 */
+/* 44 */	NdrFcShort( 0x18 ),	/* x86 Stack size/offset = 24 */
+/* 46 */	0x32,		/* FC_BIND_PRIMITIVE */
+			0x0,		/* 0 */
+/* 48 */	NdrFcShort( 0x0 ),	/* x86 Stack size/offset = 0 */
+/* 50 */	NdrFcShort( 0x8 ),	/* 8 */
+/* 52 */	NdrFcShort( 0x0 ),	/* 0 */
+/* 54 */	0x42,		/* Oi2 Flags:  clt must size, has ext, */
+			0x2,		/* 2 */
+/* 56 */	0xa,		/* 10 */
+			0x5,		/* Ext Flags:  new corr desc, srv corr check, */
+/* 58 */	NdrFcShort( 0x0 ),	/* 0 */
+/* 60 */	NdrFcShort( 0x1 ),	/* 1 */
+/* 62 */	NdrFcShort( 0x0 ),	/* 0 */
+/* 64 */	NdrFcShort( 0x0 ),	/* 0 */
+
+	/* Parameter Size */
+
+/* 66 */	NdrFcShort( 0x48 ),	/* Flags:  in, base type, */
+/* 68 */	NdrFcShort( 0x8 ),	/* x86 Stack size/offset = 8 */
+/* 70 */	0x8,		/* FC_LONG */
+			0x0,		/* 0 */
+
+	/* Parameter pBuffer */
+
+/* 72 */	NdrFcShort( 0x10b ),	/* Flags:  must size, must free, in, simple ref, */
+/* 74 */	NdrFcShort( 0x10 ),	/* x86 Stack size/offset = 16 */
+/* 76 */	NdrFcShort( 0xa ),	/* Type Offset=10 */
+
+	/* Procedure GetEntitiesFile */
+
+/* 78 */	0x0,		/* 0 */
+			0x48,		/* Old Flags:  */
+/* 80 */	NdrFcLong( 0x0 ),	/* 0 */
+/* 84 */	NdrFcShort( 0x2 ),	/* 2 */
+/* 86 */	NdrFcShort( 0x18 ),	/* x86 Stack size/offset = 24 */
+/* 88 */	0x32,		/* FC_BIND_PRIMITIVE */
+			0x0,		/* 0 */
+/* 90 */	NdrFcShort( 0x0 ),	/* x86 Stack size/offset = 0 */
+/* 92 */	NdrFcShort( 0x1c ),	/* 28 */
+/* 94 */	NdrFcShort( 0x1c ),	/* 28 */
+/* 96 */	0x41,		/* Oi2 Flags:  srv must size, has ext, */
+			0x2,		/* 2 */
+/* 98 */	0xa,		/* 10 */
+			0x3,		/* Ext Flags:  new corr desc, clt corr check, */
+/* 100 */	NdrFcShort( 0x1 ),	/* 1 */
+/* 102 */	NdrFcShort( 0x0 ),	/* 0 */
+/* 104 */	NdrFcShort( 0x0 ),	/* 0 */
+/* 106 */	NdrFcShort( 0x0 ),	/* 0 */
+
+	/* Parameter Size */
+
+/* 108 */	NdrFcShort( 0x158 ),	/* Flags:  in, out, base type, simple ref, */
+/* 110 */	NdrFcShort( 0x8 ),	/* x86 Stack size/offset = 8 */
+/* 112 */	0x8,		/* FC_LONG */
+			0x0,		/* 0 */
+
+	/* Parameter pBuffer */
+
+/* 114 */	NdrFcShort( 0x113 ),	/* Flags:  must size, must free, out, simple ref, */
+/* 116 */	NdrFcShort( 0x10 ),	/* x86 Stack size/offset = 16 */
+/* 118 */	NdrFcShort( 0x1e ),	/* Type Offset=30 */
+
+	/* Procedure GetSpawnInfo */
+
+/* 120 */	0x0,		/* 0 */
+			0x48,		/* Old Flags:  */
+/* 122 */	NdrFcLong( 0x0 ),	/* 0 */
+/* 126 */	NdrFcShort( 0x3 ),	/* 3 */
+/* 128 */	NdrFcShort( 0x18 ),	/* x86 Stack size/offset = 24 */
+/* 130 */	0x32,		/* FC_BIND_PRIMITIVE */
+			0x0,		/* 0 */
+/* 132 */	NdrFcShort( 0x0 ),	/* x86 Stack size/offset = 0 */
+/* 134 */	NdrFcShort( 0x1c ),	/* 28 */
+/* 136 */	NdrFcShort( 0x1c ),	/* 28 */
+/* 138 */	0x41,		/* Oi2 Flags:  srv must size, has ext, */
+			0x2,		/* 2 */
+/* 140 */	0xa,		/* 10 */
+			0x3,		/* Ext Flags:  new corr desc, clt corr check, */
+/* 142 */	NdrFcShort( 0x1 ),	/* 1 */
+/* 144 */	NdrFcShort( 0x0 ),	/* 0 */
+/* 146 */	NdrFcShort( 0x0 ),	/* 0 */
+/* 148 */	NdrFcShort( 0x0 ),	/* 0 */
+
+	/* Parameter Size */
+
+/* 150 */	NdrFcShort( 0x158 ),	/* Flags:  in, out, base type, simple ref, */
+/* 152 */	NdrFcShort( 0x8 ),	/* x86 Stack size/offset = 8 */
+/* 154 */	0x8,		/* FC_LONG */
+			0x0,		/* 0 */
+
+	/* Parameter pBuffer */
+
+/* 156 */	NdrFcShort( 0x113 ),	/* Flags:  must size, must free, out, simple ref, */
+/* 158 */	NdrFcShort( 0x10 ),	/* x86 Stack size/offset = 16 */
+/* 160 */	NdrFcShort( 0x1e ),	/* Type Offset=30 */
+
+			0x0
+        }
+    };
+
+static const meathook_interface_MIDL_TYPE_FORMAT_STRING meathook_interface__MIDL_TypeFormatString =
+    {
+        0,
+        {
+			NdrFcShort( 0x0 ),	/* 0 */
+/*  2 */	
+			0x11, 0x8,	/* FC_RP [simple_pointer] */
+/*  4 */	
+			0x22,		/* FC_C_CSTRING */
+			0x5c,		/* FC_PAD */
+/*  6 */	
+			0x11, 0x0,	/* FC_RP */
+/*  8 */	NdrFcShort( 0x2 ),	/* Offset= 2 (10) */
+/* 10 */	
+			0x1b,		/* FC_CARRAY */
+			0x0,		/* 0 */
+/* 12 */	NdrFcShort( 0x1 ),	/* 1 */
+/* 14 */	0x28,		/* Corr desc:  parameter, FC_LONG */
+			0x0,		/*  */
+/* 16 */	NdrFcShort( 0x8 ),	/* x86 Stack size/offset = 8 */
+/* 18 */	NdrFcShort( 0x1 ),	/* Corr flags:  early, */
+/* 20 */	0x2,		/* FC_CHAR */
+			0x5b,		/* FC_END */
+/* 22 */	
+			0x11, 0x8,	/* FC_RP [simple_pointer] */
+/* 24 */	0x8,		/* FC_LONG */
+			0x5c,		/* FC_PAD */
+/* 26 */	
+			0x11, 0x0,	/* FC_RP */
+/* 28 */	NdrFcShort( 0x2 ),	/* Offset= 2 (30) */
+/* 30 */	
+			0x1b,		/* FC_CARRAY */
+			0x0,		/* 0 */
+/* 32 */	NdrFcShort( 0x1 ),	/* 1 */
+/* 34 */	0x28,		/* Corr desc:  parameter, FC_LONG */
+			0x54,		/* FC_DEREFERENCE */
+/* 36 */	NdrFcShort( 0x8 ),	/* x86 Stack size/offset = 8 */
+/* 38 */	NdrFcShort( 0x1 ),	/* Corr flags:  early, */
+/* 40 */	0x2,		/* FC_CHAR */
+			0x5b,		/* FC_END */
+
+			0x0
+        }
+    };
+
+static const unsigned short meathook_interface_FormatStringOffsetTable[] =
+    {
+    0,
+    36,
+    78,
+    120
+    };
+
+
+static const MIDL_STUB_DESC meathook_interface_StubDesc = 
+    {
+    (void *)& meathook_interface___RpcServerInterface,
+    MIDL_user_allocate,
+    MIDL_user_free,
+    0,
+    0,
+    0,
+    0,
+    0,
+    meathook_interface__MIDL_TypeFormatString.Format,
+    1, /* -error bounds_check flag */
+    0x50002, /* Ndr library version */
+    0,
+    0x801026e, /* MIDL Version 8.1.622 */
+    0,
+    0,
+    0,  /* notify & notify_flag routine table */
+    0x1, /* MIDL flag */
+    0, /* cs routines */
+    0,   /* proxy/server info */
+    0
+    };
+
+static const RPC_DISPATCH_FUNCTION meathook_interface_table[] =
+    {
+    NdrServerCall2,
+    NdrServerCall2,
+    NdrServerCall2,
+    NdrServerCall2,
+    0
+    };
+static const RPC_DISPATCH_TABLE meathook_interface_v1_0_DispatchTable = 
+    {
+    4,
+    (RPC_DISPATCH_FUNCTION*)meathook_interface_table
+    };
+
+static const SERVER_ROUTINE meathook_interface_ServerRoutineTable[] = 
+    {
+    (SERVER_ROUTINE)ExecuteConsoleCommand,
+    (SERVER_ROUTINE)PushEntitiesFile,
+    (SERVER_ROUTINE)GetEntitiesFile,
+    (SERVER_ROUTINE)GetSpawnInfo
+    };
+
+static const MIDL_SERVER_INFO meathook_interface_ServerInfo = 
+    {
+    &meathook_interface_StubDesc,
+    meathook_interface_ServerRoutineTable,
+    meathook_interface__MIDL_ProcFormatString.Format,
+    meathook_interface_FormatStringOffsetTable,
+    0,
+    0,
+    0,
+    0};
+#if _MSC_VER >= 1200
+#pragma warning(pop)
+#endif
+
+
+#endif /* defined(_M_AMD64)*/
+
